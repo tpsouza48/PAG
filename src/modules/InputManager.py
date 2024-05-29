c57ffcbe8d8d7e1a4bff22cb18328d6b39036502
Thiago Souza <tpimentel368@gmail.com>
2024-05-29 15:57:50 -0300

    Better documentation
    Improved documentation by using docstrings

--- src/modules/InputManager.py
+++ src/modules/InputManager.py
@@ -3,11 +3,21 @@ class InputManager():
         self.icon = icon
 
     # Used all across functions to accept and validate string-only input
-    def strInput(self, text) -> str:
-        inp = str(input(text))
+    def strInput(self, text, errMsg="You should type text. Try again!") -> str:
+        """
+        Validates and returns a string of input.
 
+        Args:
+            text (str):     The text that appears on the input function.
+            errMsg (str):   This argument allows the customization of the error message.
+
+        Returns:
+            str:    The already validated input the user entered.
+        """
+        inp = str(input(text))
+    
         if inp == "":
-            print("You should type text. Try again!")
+            print(errMsg)
             input()
             return self.strInput(text)
         else:
